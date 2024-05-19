e426d435a605a77e2ab45e04cd419fe9189412b6
Filipe Lima <fhemrique55@gmail.com>
2024-05-19 13:16:56 -0300

    Course

---
+++ StudyHubAPI/src/main/java/com/studyhubapi/StudyHubAPI/domain/entities/Course.java
@@ -0,0 +1,73 @@
+package com.studyhubapi.StudyHubAPI.domain.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String imgUrl;
+    private String imgGrayUrl;
+
+    public Course(){
+
+    }
+
+    public Course(Long id, String name, String imgUrl, String imgGrayUrl) {
+        this.id = id;
+        this.name = name;
+        this.imgUrl = imgUrl;
+        this.imgGrayUrl = imgGrayUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgGrayUrl() {
+        return imgGrayUrl;
+    }
+
+    public void setImgGrayUrl(String imgGrayUrl) {
+        this.imgGrayUrl = imgGrayUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return Objects.equals(id, course.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
--- StudyHubAPI/src/main/resources/import.sql
+++ StudyHubAPI/src/main/resources/import.sql
@@ -1,9 +1,23 @@
 INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Pedro', 'pedro@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 3);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+
+
+INSERT INTO tb_course (name, img_Url, img_Gray_Url) VALUES ('Introduction to Programming', 'http://example.com/img/programming.jpg', 'http://example.com/img/programming_gray.jpg');
+INSERT INTO tb_course (name, img_Url, img_Gray_Url) VALUES ('Advanced Databases', 'http://example.com/img/databases.jpg', 'http://example.com/img/databases_gray.jpg');
+INSERT INTO tb_course (name, img_Url, img_Gray_Url) VALUES ('Machine Learning Basics', 'http://example.com/img/machine_learning.jpg', 'http://example.com/img/machine_learning_gray.jpg');
+INSERT INTO tb_course (name, img_Url, img_Gray_Url) VALUES ('Web Development with JavaScript', 'http://example.com/img/web_dev.jpg', 'http://example.com/img/web_dev_gray.jpg');
+INSERT INTO tb_course (name, img_Url, img_Gray_Url) VALUES ('Data Structures and Algorithms', 'http://example.com/img/data_structures.jpg', 'http://example.com/img/data_structures_gray.jpg');
